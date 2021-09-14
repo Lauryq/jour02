e289b651492c76f6a0c8d3414b5b01fcdfc08353
“laury <lquarto.lq@gmail.com>
2021-09-14 16:56:43 +0200

    consigne 2 ex1 ok

--- exercice1/app.js
+++ exercice1/app.js
@@ -2,7 +2,9 @@ const btn1 = document.querySelector('.btn1');
 const btn2 = document.querySelector('.btn2');
 const btn3 = document.querySelector('.btn3');
 const btn4 = document.querySelector('.btn4');
+const input1 = document.querySelector('.input1');
 const random = document.querySelector('.random');
+const p2 = document.querySelector('.bjr');
 const main = document.querySelector('main');
 
 /*ex1*/
@@ -14,3 +16,15 @@ btn1.addEventListener('click', function(){
 });
 
 /*ex2*/
+btn2.addEventListener('click', function(){
+    let val = input1.value;
+    let regex = new RegExp("[a-zA-Z^[0-9]]");
+    
+    if ( !regex.test(val)){
+        alert('mauvais caractères');
+    }
+    else{
+        p2.innerHTML = `bonjour ${val}`;    
+    }
+})
+
